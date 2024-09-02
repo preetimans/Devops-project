c3bc1de24c0311a66d74176ef75129547bc68746
Shyamsandeep28 <56245081+Shyamsandeep28@users.noreply.github.com>
2024-09-02 15:18:49 +0530

    Update LoginServlet.java

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -41,6 +41,7 @@ public class LoginServlet extends HttpServlet {
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<h1> Hi , HI HSBC Team , This is jenkins Job </h1>");
+		out.println("<h1> Hi , HI HSBC Team , This is update file</h1>");
 		out.println("</body>");
 		out.println("</html>");
 
